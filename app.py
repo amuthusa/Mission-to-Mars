abd533da681f7af1262064fc7d0042572313fb12
Ayyappa Muthusami <ayyappamuthusami@Ayyappas-MacBook-Pro.local>
2020-03-21 19:46:20 -0700

    fixed flask application, added template html file which uses flask variable to get the data from mongodb

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #import flask related dependency
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-import scraping
+import Scraping
 
 #set flask app
 app = Flask(__name__)
@@ -15,11 +15,12 @@ def index():
     mars = mongo.db.mars.find_one()
     return render_template("index.html", mars=mars)
 
-@app.route("/scrape")
+@app.route("/Scrape")
 def scrape():
     mars = mongo.db.mars
-    mars_data = scraping.scrape_all()
+    mars_data = Scraping.scrape_all()
     mars.update({}, mars_data, upsert=True)
+    #mars.insert(mars_data)
     return "Scraping successful!"
 
 if __name__ == "__main__":
